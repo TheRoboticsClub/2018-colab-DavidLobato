29bbcce1a2e93585e7fb765e35e5327a3d56ec91
dlobato <dav.lobato@gmail.com>
2018-10-23 16:28:41 +0100

    Update xor mlp with keras

--- mlp_examples/xor/xor_mlp_keras.py
+++ mlp_examples/xor/xor_mlp_keras.py
@@ -1,60 +1,61 @@
 import os
+import argparse
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.contrib import quantize
 from tensorflow.python import debug as tf_debug
+from tensorflow.python.util import serialization
 
 # Helper libraries
 import numpy as np
-import matplotlib.pyplot as plt
+import json
+
+parser = argparse.ArgumentParser(description='XOR MLP train and evaluation.')
+parser.add_argument('-t', '--train', action='store_true', help='Train XOR MLP')
+parser.add_argument('-d', '--dump-variables', metavar='PATH', help='Dump variables to file')
+
+args = parser.parse_args()
 
 sess = tf.Session()
-
-debug = False
-if debug:
-    sess = tf_debug.TensorBoardDebugWrapperSession(sess, 'localhost:6064')
 keras.backend.set_session(sess)
 
-graph_path = os.path.join("models", "xor_mlp.pbtxt")
 checkpoint_prefix = os.path.join("models", "xor_mlp.ckpt")
 
 (data, labels) = (
     np.array([[0, 0], [0, 1], [1, 0], [1, 1]]),
     np.array([[0], [1], [1], [0]]) # xor
-    # numpy.array([[1], [0], [0], [1]]) # xnor
+    #np.array([[1], [0], [0], [1]]) # xnor
 )
 
-my_init = keras.initializers.glorot_uniform(seed=1)
+glorot_init = keras.initializers.glorot_uniform()
 
 model = keras.Sequential([
-    keras.layers.Dense(5, input_dim=2, activation=tf.nn.relu, kernel_initializer=my_init),
-    keras.layers.Dense(1, activation=tf.nn.sigmoid, kernel_initializer=my_init)
+    keras.layers.Dense(5, input_dim=2, activation=tf.nn.relu, kernel_initializer=glorot_init),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid, kernel_initializer=glorot_init)
 ])
 
-quantize.experimental_create_training_graph(weight_bits=8, activation_bits=8, quant_delay=5000)
+if args.train:
+    quantize.experimental_create_training_graph(weight_bits=8, activation_bits=8, quant_delay=5000)
 
-model.compile(optimizer=tf.keras.optimizers.Adam(),
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
            loss=tf.keras.losses.binary_crossentropy,
            metrics=['accuracy'])
+    sess.run(tf.global_variables_initializer())
+    model.fit(data, labels, epochs=10000)
+    tf.train.Saver().save(sess, checkpoint_prefix)
 
-callbacks = [
-    # Write TensorBoard logs to `./logs` directory
-    keras.callbacks.TensorBoard(log_dir='./logs/xor_mlp'),
-    #keras.callbacks.ModelCheckpoint(checkpoint_path, monitor='val_loss', save_best_only=True)
-]
-sess.run(tf.global_variables_initializer())
+    test_loss, test_acc = model.evaluate(data, labels)
 
-saver = tf.train.Saver()
+    print('Test accuracy:', test_acc)
+else:
+    quantize.experimental_create_eval_graph(weight_bits=8, activation_bits=8)
+    tf.train.Saver().restore(sess, checkpoint_prefix)
+    print('Prediction:', model.predict(data))
 
-model.fit(data, labels, epochs=10000, callbacks=callbacks)
-
-# Save the checkpoint and eval graph proto to disk for freezing
-with open(graph_path, 'w') as f:
-    f.write(str(sess.graph.as_graph_def()))
-
-saver.save(sess, checkpoint_prefix)
-
-test_loss, test_acc = model.evaluate(data, labels)
-
-print('Test accuracy:', test_acc)
+if args.dump_variables:
+    variables = {}
+    for tf_variable in tf.global_variables():
+        variables[tf_variable.name] = sess.run(tf_variable)
+    with open(args.dump_variables, 'w') as f:
+        json.dump(variables, f, ensure_ascii=False, default=serialization.get_json_type)
